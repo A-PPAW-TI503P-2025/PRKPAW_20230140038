8be89c5b4ef35c2cd05ca237a7bf9ec6b37f60a9
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-21 20:29:09 +0700

    Tugas 3

---
+++ nim-node-server/routes/books.js
@@ -0,0 +1,1 @@
+// dummy books route
---
+++ nim-node-server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3001; 
+const morgan = require("morgan");
+
+const presensiRoutes = require("./routes/presensi"); 
+const reportRoutes = require("./routes/reports");   
+const ruteBuku = require("../routes/books");         
+
+app.use(cors());           
+app.use(express.json());   
+app.use(morgan("dev"));    
+
+app.use((req, res, next) => { 
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => { 
+  res.send("Home Page for API");
+});
+
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes); 
+app.use("/api/reports", reportRoutes);   
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`); 
+});
